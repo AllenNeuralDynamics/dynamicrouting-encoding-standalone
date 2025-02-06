9c30693ea81e61c97b3e1abaff88455b20bf3d03
shailajaAkella <shailaja.akella@alleninstitute.org>
2025-02-06 03:29:21 +0000

    Update storage of design_mat

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -168,9 +168,9 @@ def process_session(session_id: str, params: "Params", test: int = 0) -> None:
 
         np.savez(
             file=output_path,
-            design_matrix=design_mat.data,
-            design_matrix_weight_labels = design_mat.weights,
-            design_matrix_timestamps = design_mat.timestamps, 
+            design_matrix= {'data' = design_mat.data,
+                            'weight_labels' = design_mat.weights,
+                            'timestamps' = design_mat.timestamps}, 
             fit=fit,  # Ensure dict can be saved properly
             run_params=run_params,  # Ensure dict can be saved properly
         )
